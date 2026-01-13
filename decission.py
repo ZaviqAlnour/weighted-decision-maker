db72241239e569d5038c6ae96800558dab5910ec
Zaviq Alnour <181534609+ZaviqAlnour@users.noreply.github.com>
2026-01-13 08:18:02 +0600

    Added criteria function

--- decission.py
+++ decission.py
@@ -1,6 +1,7 @@
 def main():
     decision, option_count, criteria_count = firstAsk()
     askOption(option_count)
+    askCriteria(criteria_count)
     
 
 def firstAsk():
@@ -16,8 +17,15 @@ def askOption(option_count):
     options = []
     for i in range(option_count):
         options.append(input(f"Enter option {i + 1}: "))
-   
 
+    return options    
+   
+def askCriteria(criteria):
+    criterias = []
+    for i in range(criteria):
+        criterias.append(input(f"Enter criteria {i + 1}: "))
+
+    return criterias    
 
 
 
