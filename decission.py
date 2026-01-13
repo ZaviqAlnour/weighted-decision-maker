3e95dd480f3eb06de396f6e3461693a390792f71
Zaviq Alnour <181534609+ZaviqAlnour@users.noreply.github.com>
2026-01-13 10:02:49 +0600

    Fix bugs in the collect_criteria_points function

--- decission.py
+++ decission.py
@@ -1,8 +1,8 @@
 def main():
     decision, option_count, criteria_count = firstAsk()
     options = askOption(option_count)
-    askCriteria(criteria_count)
-    collect_options_points(options)
+    criterias = askCriteria(criteria_count)
+    collect_criteria_points(options, criterias)
     
 
 def firstAsk():
@@ -18,31 +18,37 @@ def askOption(option_count):
     options = []
     for i in range(option_count):
         options.append(input(f"Enter option {i + 1}: "))
-
+    print()    
     return options    
    
 def askCriteria(criteria):
     criterias = []
     for i in range(criteria):
         criterias.append(input(f"Enter criteria {i + 1}: "))
-
+    print()
     return criterias    
 
-def collect_options_points(options):
-    options_point = []
+def collect_criteria_points(options, criteria):
+    criteria_point = []
+    op_index = 0
     for op in options:
-        while True:
-            try:
-                op_point = int(input(f"Enter point (0 - 10) for {op}: "))
-                if op_point < 0 or op_point > 10:
-                    print("You have enter point between (0 - 10).")
-                    continue
-                else:
-                    break
-            except ValueError:
-                print("This is not valid you have to give a number.")
-                continue    
-    options_point.append(op_point)
+        print(f"\n------ point for {options[op_index]} --------\n")
+
+        for cr in criteria:
+            while True:
+                try:
+                    op_point = int(input(f"Enter point (0 - 10) for {cr}: "))
+                    if op_point < 0 or op_point > 10:
+                        print("You have enter point between (0 - 10).")
+                        continue
+                    else:
+                        break
+                except ValueError:
+                    print("This is not valid you have to give a number.")
+                    continue 
+        op_index += 1       
+
+    criteria_point.append(op_point)
 
 
 main()
